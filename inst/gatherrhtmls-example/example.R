815a774f4e01a29cb1ac4494a57bdce35aa3390d
kpawel125 <kpawel2210@gmail.com>
2020-03-18 15:04:24 +0100

    Example updated

--- inst/gatherrhtmls-example/example.R
+++ inst/gatherrhtmls-example/example.R
@@ -1,10 +1,5 @@
 # devtools::install_github("pawel125/gatherrhtmls")
 library(gatherrhtmls)
-files <- get_htmls()
-gather(files, output_dir = "report")
-
-# or simply to gather all reports in the project directory
-gather(output_dir = "report")
-
-#
 generate_yaml()
+gather()
+
